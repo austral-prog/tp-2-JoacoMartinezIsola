41891f44173d84105c532d4f143fb0cda18ff822
JoacoMartinezIsola <jmartinezisola@mail.austral.edu.ar>
2025-04-01 15:44:22 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    Vuelto = (money-expense)
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("\nVuelto")
+    print("\nPesos:")
+    print(int(Vuelto))
+    print("Centavos:")
+    print(round((Vuelto-int(Vuelto))*100))
+
+change()
