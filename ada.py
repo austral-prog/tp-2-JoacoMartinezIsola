ce12541d29fec1ebb3aa31a0257e9c1159772d48
JoacoMartinezIsola <jmartinezisola@mail.austral.edu.ar>
2025-04-01 15:43:34 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name="Ada Lovelace"
+    print(name.lower())
+    print(name)
+    print(name.upper())
+    name_lower=f"\t{name.lower()}"
+    print(name_lower)
+ada()
